c09fdb651adf0199b353fd7229ecdc2c8b6cc552
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-12-22 23:26:24 +0500

    max number practice

--- basic_logic_building.js
+++ basic_logic_building.js
@@ -38,7 +38,44 @@ for(let i = 0; i<=20; i++){
 console.log('Sum of first 10 natural numbers');
 let sum = 0;
 for(let i=1; i<=10; i++){
-    add = sum+i;
-    console.log(add)
+    sum +=i;
+}
+console.log(sum);
+
+
+//
+console.log('Multiplication table of 5');
+let num = 5;
+for(let i=1; i<=10; i++){
+    console.log(num ,'*', i ,'=', num*i)
 }
 
+
+//
+console.log('List Print');
+const list = [20,30,40,50,60]
+for(let i = 0; i<list.length; i++){
+    console.log(list[i])
+}
+
+
+console.log('find max number');
+
+const arr = [20, 30, 40, 50, 60];
+
+// assume first element is max
+let max = arr[0];
+
+// start loop from second element
+for (let i = 1; i < arr.length; i++) {
+    // compare current element with max
+    if (arr[i] > max) {
+        // update max
+        max = arr[i];
+    }
+}
+
+// print result
+console.log("Max number is:", max);
+
+    
